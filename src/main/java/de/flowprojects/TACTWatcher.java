16efbd5fa28417655b559d82b0c4467898d88895
Florian Riedlinger <florian.riedlinger@live.de>
2023-04-25 23:02:09 +0200

    added readTempTACTFile

--- src/main/java/de/flowprojects/TACTWatcher.java
+++ src/main/java/de/flowprojects/TACTWatcher.java
@@ -4,16 +4,41 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.stream.Stream;
 
 public class TACTWatcher {
-    public static void main(String[] args) {
-        //System.out.println("Hello world!");
+    public static void main(String[] args) throws MalformedURLException {
+        final String WOWDEV2_TACT_CONTENT_URL = "http://us.patch.battle.net:1119/wowdev2/versions";
+        File tempTACTFile = getTACTFileContent(WOWDEV2_TACT_CONTENT_URL);
+
+        if(tempTACTFile != null) {
+            readTempTACTFile(tempTACTFile);
+        }
+    }
+
+
+    private static File createTempTACTFile() throws IOException {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd_hhmmss");
+        String filename = "tactfile_" + date.format(new Date());
+        File tempFile = File.createTempFile(filename, ".txt", new File("temp"));
+        tempFile.deleteOnExit();
+
+        return tempFile;
+    }
+
+    /**
+     * Get content from TACT URL and write it into temporary file
+     * (see <a href="https://wowdev.wiki/TACT#HTTP_URLs">here</a>)
+     * @param tactContentURL TACT URL
+     */
+    private static File getTACTFileContent(String tactContentURL) {
         try {
             File file = createTempTACTFile();
 
-            URL url = new URL("http://us.patch.battle.net:1119/wowdev2/versions");
+            URL url = new URL(tactContentURL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             InputStream input = con.getInputStream();
 
@@ -27,16 +52,19 @@ public class TACTWatcher {
             }
             output.close();
 
-
+            return file;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return null;
     }
 
-    private static File createTempTACTFile() throws IOException {
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd_hhmmss");
-        String filename = "tactfile_" + date.format(new Date());
-        return File.createTempFile(filename, ".txt", new File("temp"));
+    private static void readTempTACTFile(File tactFile) {
+        try(Stream<String> lines = Files.lines(tactFile.toPath())) {
+            System.out.println("Reading file content:\n");
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
